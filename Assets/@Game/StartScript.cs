597ba7fd4d1c941fe96c8ab62fe66a5dba3f1dc9
mcarr390 <mcarr390@gmail.com>
2025-08-20 09:01:39 -0700

    Fixed previous merge conflic issues

--- Assets/@Game/StartScript.cs
+++ Assets/@Game/StartScript.cs
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 using tasks_and_resources.Core;
-=======
->>>>>>> 2754e25 (Setup odin, setup game actions/resources)
 using UnityEngine;
 
 namespace Game
@@ -10,25 +7,12 @@ namespace Game
     {
         void Start()
         {
-<<<<<<< HEAD
-            ResourceRegistry.Init();
-            
-            TaskRegistry.Init(ResourceRegistry.Registry.Values);
 
-            foreach (var res in ResourceRegistry.Registry)
-            {
-                //Debug.Log(res.Value.Name);
-            }
         }
 
-    
-=======
-            
-            
-            
-            
-        }
-        
->>>>>>> 2754e25 (Setup odin, setup game actions/resources)
+
+
     }
+
+
 }
